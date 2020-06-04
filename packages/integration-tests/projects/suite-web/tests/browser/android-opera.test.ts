91b52d316c2835e3f08d11cf314b6162a81d97be
mroz22 <mvarmuza@gmail.com>
2020-06-04 16:02:20 +0200

    opera-mini on android

---
+++ packages/integration-tests/projects/suite-web/tests/browser/android-opera.test.ts
@@ -0,0 +1,14 @@
+// @beta
+// @user-agent=Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10
+
+describe('Android with Opera-mini browser ', () => {
+    before(() => {
+        cy.task('stopEmu');
+        cy.viewport('samsung-s10');
+        cy.resetDb();
+    });
+
+    it('Should display "Get Chrome for Android"', () => {
+        cy.get('body').should('contain.text', 'Get Chrome for Android');
+    })
+})
