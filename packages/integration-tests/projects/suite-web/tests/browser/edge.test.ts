a00e4a8e729a7216021abf66029c987366c0ef37
mroz22 <mvarmuza@gmail.com>
2020-06-04 15:34:19 +0200

    edge

---
+++ packages/integration-tests/projects/suite-web/tests/browser/edge.test.ts
@@ -0,0 +1,15 @@
+// @beta
+// @user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246
+
+describe('Windows 10 with edge browser ', () => {
+    before(() => {
+        cy.task('stopEmu');
+        cy.viewport(1024, 768);
+        cy.resetDb();
+    });
+
+    it('Should display unsupported browsers page', () => {
+        cy.prefixedVisit('/');
+        cy.get('html').should('contain.text', 'Your browser is not supported');
+    });
+})
---
+++ packages/integration-tests/projects/suite-web/tests/browser/outdated-firefox.test.ts
@@ -0,0 +1,17 @@
+// @beta
+// @user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1
+
+describe('Ubuntu with outdated firefox ', () => {
+    before(() => {
+        cy.task('stopEmu');
+        cy.viewport(1024, 768);
+        cy.resetDb();
+    });
+
+    it('Should just display outdated browser', () => {
+        cy.prefixedVisit('/');
+        cy.get('html').should('contain.text', 'Your browser is outdated');
+        cy.get('a').click();
+        cy.getTestElement('fooo');
+    });
+})
